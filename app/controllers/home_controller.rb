b5df5a8a24766809217290cc0e53e0b95864dec2
v1nnyc <cannallav019@yahoo.com>
2017-10-10 13:04:34 -0700

    working on transfering css and js files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
 
   # Default index homepage for website.
   def index
+    # Redirect to login for now.
+
   end
 
   # Shared page with various Options.
